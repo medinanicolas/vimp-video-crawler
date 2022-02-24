f6207528559f459bdd762a28ca2a7c2de35db06b
Nicolas Medina <medinanicolas@protonmail.com>
2022-02-23 23:20:42 -0300

    update

--- README.md
+++ README.md
@@ -1,20 +1,8 @@
 # VVCrawler 🕷
 
-_Vimp Video Crawler es una herramienta de web crawler para la descarga de archivos de video dentro de páginas que ocupen servicios VIMP_
+_Vimp Video Crawler es una herramienta de web scraping para la descarga de archivos de video dentro de páginas que ocupen servicios VIMP_
 
 _Está basada en expresiones regulares_
-
-## Caracteristicas
-
-* Descarga videos en un rango de búsqueda
-
-    ### Proximas caracteristicas y mejoras
-
-    * Preguntar por directorio de descarga
-    * Descarga individual
-    * Búsqueda dentro del programa
-    * Reintentar descargas en caso de error
-    * Pulido 💎
     
 ## Dependencias
 
@@ -25,18 +13,14 @@ _Está basada en expresiones regulares_
 # Uso
 _vvcrawler se ejecuta mediante linea de comandos_
 
-_Al ejecutarlo se debe establecer un enlace de búsqueda dentro de VIMP_
+`$ python3 vvcrawler.py -u URL`
 
-_La herramienta analizará todas las páginas de la búsqueda y procederá a descargar los videos_
+_Al ejecutarlo se debe establecer un enlace de búsqueda dentro de VIMP o un enlace de video individual_
 
-_Creará una carpeta por cada archivo con su respectivo nombre_
+_En caso de ser una búsqueda, la herramienta analizará todas las páginas de la búsqueda y procederá a descargar los videos_
+
 
 ![demo](demo/demo.gif)
-
-
-### Pasos a seguir
-   1. Establecer enlace de búsqueda VIMP _(p.e. https://vimp.vv/search/title/Search/description/Search/tags/Search/type/all/search/basic/categoriesopt/0)_
-   1. Esperar.. 🧉
 
 ## Licencia
 [LICENCE](docs/LICENCE) - GNU General Public License v3.0 
--- vvcrawler.py
+++ vvcrawler.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-#import argparse
-from urllib.request import urlopen,urlretrieve
+from urllib.request import urlopen, urlretrieve
 import urllib.parse
-import os
+import os, re, sys, argparse, random, string
 from bs4 import BeautifulSoup
-import re
 from time import sleep
 from colorama import init, Fore, Style
 #---------------------------------------------------------------------------------
@@ -15,60 +13,100 @@ print(Fore.MAGENTA+Style.BRIGHT+r"""
  ;_/,L-,\_;     VIMP Video Crawler
 \._/3  E\_./    
 \_./(::)\._/
-     ''
+     ""
 """)
-while True:
-    print("Pulse X en cualquier momento para salir")
-    url=input(Fore.WHITE+'[+] Ingrese url de búsqueda: ')
-    if url.upper()=='X':
-        exit(0)
-    if url.startswith('https') or url.startswith('http'):
-        urlsplt = re.split('/+',url)
-        hostname = urlsplt[0]+'//'+urlsplt[1]
-    else:
-        hostname = url.split('/')[0]
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-u", "--url", help="Search or video URL to download", metavar="URL", required=True)
+argparser.add_argument("-o", "--out", help="Output directory", metavar="DIR")
+args = argparser.parse_args()
+
+def download(link):
     try:
-        req=urlopen(url)
+        req = urlopen(link)
     except Exception as ex:
-        print(Fore.RED+'[!] Verifique que el URL esté correcto o revise su conexión')
-        continue
-    first_page = req.geturl()
-    html = str(req.read())
-    print(Fore.YELLOW+'[#] Buscando páginas...')
-    link_pages=re.search('(?<=<!-- pages -->).*?(?=<!-- next page -->)',html,re.DOTALL)
-    links_html = link_pages.group(0)
-    soup =BeautifulSoup(links_html,'html.parser')
-    tags = soup('a')
-    links = list()
-    links.insert(0,first_page)
+        print(Fore.RED+"[!] Ha ocurrido un error encontrando los videos:",ex)
+        sys.exit()
+    html = req.read()
+    soup = BeautifulSoup(html,"html.parser")
+    tags = soup.find_all(attrs={"class":"mediaInfo"})
+    
+    if not tags:
+        print(Fore.RED+"[!] No se encontraron videos para descargar.")
+        sys.exit() 
+
     for tag in tags:
-        links.append(tag.get('href',None))
-    npages=len(links)
-    print(Fore.CYAN+'[*] La busqueda cuenta con '+str(npages)+' páginas.')    
-    for link in links:
-        print(Fore.MAGENTA+'[#] Buscando videos en la página '+str(links.index(link)+1))
+        regex = "[a-z0-9]{32}"
+        video_hash = re.search(regex, url)
+
+        if video_hash:
+            title = urlsplt[-2]
+            video_hash = video_hash.group(0)
+        else:
+            link = tag.a.get("href", None)
+            video_hash = re.search(regex, link).group(0)
+            if not video_hash:
+                print(Fore.RED+"[!] No se encontró el hash del video.")
+                sys.exit()
+            title=link.split("/")[-2]
+
+        video_link = video_link = hostname+"/getMedium/"+video_hash+".m4v"
+
+        if args.out:
+            out = args.out
+        else:
+            out = title
+        if not os.path.exists(out):
+            os.makedirs(out)
+
         try:
-            req=urlopen(link)
+            print(Fore.BLUE+"[#] Descargando "+title+"...")
+
+            if os.path.exists(os.path.join(out, title+".m4v")):
+                print(Fore.YELLOW+"[!] "+title+" ya existe, añadiendo carácteres aleatorios.")
+                title += "_" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(8))
+
+            urllib.request.urlretrieve(video_link, out + "/" + title + ".m4v")
+            print(Fore.GREEN+"[#] " + title + " descargado.")
+            sleep(1)
         except Exception as ex:
-            print(Fore.RED+'[!] Ha ocurrido un error buscando los videos:',ex)
-            break
-        html = req.read()
-        soup = BeautifulSoup(html,'html.parser')
-        tags = soup.find_all(attrs={"class":"mediaInfo"})
-        for tag in tags:
-            link = tag.a.get('href',None)
-            link = link.split('/')
-            video_hash = link[-1]
-            video_link=hostname+'/getMedium/'+video_hash+'.m4v'
-            title=link[2]
-            if not os.path.exists(title):
-                os.makedirs(title)
-            try:
-                print(Fore.YELLOW+'[#] Descargando '+title+'...')
-                urllib.request.urlretrieve(video_link,title+'/'+title+'.m4v')
-                print(Fore.GREEN+'[*] '+title+' descargado.')
-                sleep(1)
-            except Exception as ex:
-                print(Fore.RED+'[!] Ha ocurrido un error descargando los videos:',ex)
-                break
-    print(Fore.CYAN+'[*] Todos los videos se han descargado satisfactoriamente.')
+            print(Fore.RED+"[!] Ha ocurrido un error descargando los videos:", ex)
+            sys.exit()
+
+url = args.url
+
+if url[:5].lower() == "https" or url[:4].lower() == "http":
+    urlsplt = re.split("/+", url)
+    hostname = urlsplt[0]+"//"+urlsplt[1]
+else:
+    hostname = url.split("/")[0]
+try:
+    req = urlopen(url)
+except Exception as ex:
+    print(Fore.RED+"[!] Verifique que el URL esté correcto o revise su conexión")
+    sys.exit()
+
+first_page = req.geturl()
+
+html = req.read().decode()
+
+print(Fore.BLUE+"[#] Buscando páginas...")
+link_pages = re.search("(?<=<!-- pages -->).*?(?=<!-- next page -->)", html, re.DOTALL)    
+
+if link_pages:
+    links_html = link_pages.group(0)
+    soup = BeautifulSoup(links_html, "html.parser")
+    tags = soup("a")
+
+    links = [tag.get("href", None) for tag in tags]
+    links.insert(0, first_page)
+
+    print(Fore.BLUE+"[#] La busqueda cuenta con " + str(len(links)) + " páginas.")  
+    
+
+    for link in links:
+        print(Fore.BLUE+"[#] Buscando videos en la página "+str(links.index(link)+1))
+        download(link)
+else:
+    print(Fore.YELLOW+"[!] No se han encontrado páginas, intentando como video individual...")
+    download(url)
